8cea7dded66d2b14e603d566394610f415202770
evanhackett <evanwhackett@gmail.com>
2015-03-04 15:36:14 -0800

    completed reject

--- src/underbar.js
+++ src/underbar.js
@@ -93,6 +93,10 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(item) {
+      if (!test(item)) return true;
+      else return false;
+    });
   };
 
   // Produce a duplicate-free version of the array.
